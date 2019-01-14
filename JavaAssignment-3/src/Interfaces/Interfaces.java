9fe56e7627b9cb6f714e226f5c7336a1695e900e
PoojaBiyani777 <43056549+PoojaBiyani777@users.noreply.github.com>
2019-01-14 13:39:14 +0530

    Update Interfaces.java

--- JavaAssignment-3/src/Interfaces/Interfaces.java
+++ JavaAssignment-3/src/Interfaces/Interfaces.java
@@ -1,9 +1,4 @@
 package Interfaces;
-/*Create three interfaces, each with two methods. Inherit a new interface that combines the three,
- * adding a new method. Create a class by implementing the new interface and also inheriting from a
- * concrete class. Now write four methods, each of which takes one of the four interfaces as an argument.
- * In main( ), create an object of your class and pass it to each of the methods.
- */
 interface Interface1{
     void method1();
     void method2();
@@ -48,10 +43,10 @@ public class Interfaces extends SuperClass implements Interface4{
     }
 
     public static void interface1(Interface1 a){
-        a.method1();
+        a.method2();
     }
     public static void interface2(Interface2 a){
-        a.method3();
+        a.method4();
     }
     public static void interface3(Interface3 a){
         a.method5();

89c11495162464516553c13d69d5dd1193578f62
PoojaBiyani777 <43056549+PoojaBiyani777@users.noreply.github.com>
2019-02-20 12:40:45 +0530

    Update CharacterOccurences.java

--- JavaAssignment-7/src/CharacterOccurences.java
+++ JavaAssignment-7/src/CharacterOccurences.java
@@ -1,36 +1,34 @@
-import java.sql.SQLOutput;
 import java.io.*;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class CharacterOccurences {
+
+    static Logger logger = Logger.getLogger(CharacterOccurences.class.getName());
     private static final int MAX_CHAR = 256 ;
 
-    public static void main(String args[]) throws FileNotFoundException
-    {
-        int length=0;
-        String argsFileName="";
-
-        for(int i=0;i<args.length;i++){
-            argsFileName+=args[i];
-        }
-        System.out.println("Given file name path is : "+argsFileName);
-
-        File file=new File(argsFileName);
-        BufferedReader br=new BufferedReader(new FileReader(file));
-        String st;
-        String str="";
+    public static void main(String[] args) throws FileNotFoundException,IOException {
+        BufferedReader br = null;
         try {
+            int length = 0;
+            String argsString = Arrays.toString(args);
+            String filePath = argsString.substring(1, argsString.length() - 1);
+            logger.info("Given file name is "+filePath);
+
+            File file = new File(filePath);
+            br = new BufferedReader(new FileReader(file));
+            String st;
+            String str = "";
+
             while ((st = br.readLine()) != null) {
                 System.out.println(st);
                 length++;
-                str+=st;
-
+                str += st;
             }
-            System.out.println("String is : "+str);
+            logger.info("String is : " + str);
 
             int count[] = new int[MAX_CHAR];
-
             int len = str.length();
-            System.out.println("Length is : "+len);
-
             // Initialize count array index
             for (int i = 0; i < len; i++)
                 count[str.charAt(i)]++;
@@ -43,18 +41,21 @@ public class CharacterOccurences {
                 for (int j = 0; j <= i; j++) {
 
                     // If any matches found
-                    if (str.charAt(i) == ch[j])
+                    if (str.charAt(i) == ch[j]) {
                         find++;
+                    }
                 }
 
-                if (find == 1)
+                if (find == 1) {
                     System.out.println("Number of Occurrence of " +
                             str.charAt(i) + " is:" + count[str.charAt(i)]);
-            }
-        }
+                }
 
-        catch(Exception e){
+            }
+        } catch (Exception e) {
             System.out.println("IO Exception exists here!!!");
+        } finally {
+            br.close();
         }
 
     }
